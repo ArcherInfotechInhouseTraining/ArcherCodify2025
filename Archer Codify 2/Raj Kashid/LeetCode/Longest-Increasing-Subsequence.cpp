5b527102a674d33d4975f042ee143dfa255fb7f8
Itsrajsk <rajkashid146303@gmail.com>
2025-03-06 10:49:33 +0530

    Raj Added 31th Problem

---
+++ Archer Codify 2/Raj Kashid/LeetCode/Longest-Increasing-Subsequence.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int lengthOfLIS(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> LIS(n, 1);
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (nums[i] < nums[j])
+                {
+                    LIS[i] = max(LIS[i], 1 + LIS[j]);
+                }
+            }
+        }
+        return *max_element(LIS.begin(), LIS.end());
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int> nums = {10, 9, 2, 5, 3, 7, 101, 18};
+    cout << "Length of Longest Increasing Subsequence: " << sol.lengthOfLIS(nums) << endl;
+    return 0;
+}
